3f2f74bf1aeaee434ead55ad2b80cedf19632ae1
Hramov <trykhramov@gmail.com>
2021-03-24 20:44:10 +0700

    Rebuild socket system

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,62 +2,75 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"strconv"
 
 	"github.com/hramov/battleship_server/pkg/battlefield"
 	connection "github.com/hramov/battleship_server/pkg/connection"
 	"github.com/hramov/battleship_server/pkg/ship"
+	"github.com/hramov/battleship_server/pkg/utils"
 )
 
 func main() {
-	connection.Execute("tcp", "127.0.0.1", "5000",
-		func(s *connection.Server, client connection.Client, clients map[int]connection.Client) {
 
-			b := battlefield.BattleField{}
-			sh := ship.Ship{}
-			var ships []ship.Ship
+	b := battlefield.BattleField{}
+	sh := ship.Ship{}
+	var ships []ship.Ship
 
-			for {
-				/** Обрабатываем событие подключения */
-				s.On(client, "connect", func(data string) {
-					s.Emit(client, "connect", "")
-					s.Emit(client, "whoami", "")
-				})
+	clients := make(map[int]connection.Client)
 
-				s.On(client, "sendName", func(data string) {
-					s.Emit(client, "enemy", "2")
-					client.Name = data
+	s := connection.Execute("tcp", "127.0.0.1", "5000")
 
-					s.Emit(client, "drawField", "")
-					s.Emit(client, "placeShip", "")
-				})
+	handlers := make(map[string]func(client connection.Client, data string))
 
-				s.On(client, "sendShip", func(data string) {
-					json.Unmarshal([]byte(data), &sh)
-					err := b.CheckShip(sh)
-					if err != nil {
-						s.Emit(client, "wrongShip", fmt.Sprint(err))
-						s.Emit(client, "placeShip", "")
-					} else {
-						fmt.Println(sh)
-						ships = append(ships, sh)
-						if len(ships) < 10 {
-							s.Emit(client, "placeShip", "")
-						} else {
-							s.Emit(client, "hit", "")
-						}
-					}
-				})
+	handlers["connect"] = func(client connection.Client, data string) {
+		s.Emit(client, "connect", "")
+		s.Emit(client, "whoami", "")
+	}
 
-				/** Обрабатываем событие отключения */
-				s.On(client, "disconnect", func(data string) {
-					fmt.Println(client.Name + " disconnected!")
-					close(client.From)
-					close(client.To)
-					delete(clients, client.ID)
-				})
+	handlers["sendName"] = func(client connection.Client, data string) {
+		s.Emit(client, "enemy", "2")
+		s.Emit(client, "drawField", "")
+		s.Emit(client, "placeShip", "")
+	}
+
+	handlers["sendShip"] = func(client connection.Client, data string) {
+		json.Unmarshal([]byte(data), &sh)
+		err := b.CheckShip(sh)
+		if err != nil {
+			s.Emit(client, "placeShip", err.Error())
+		} else {
+			ships = append(ships, sh)
+			if len(ships) < 10 {
+				s.Emit(client, "placeShip", "")
+			} else {
+				s.Emit(client, "hit", "")
 			}
-		}) // Запуск сокет-сервера
+		}
+	}
 
-	// gameloop.Init()
+	handlers["disconnect"] = func(client connection.Client, data string) {
+		utils.Log("User: " + strconv.Itoa(client.ID) + " disconnected!")
+		client.Socket.Close()
+		delete(clients, client.ID)
+		utils.Log("Users remain: " + strconv.Itoa(len(clients)))
+	}
+
+	maintainConnections(s, &clients, &handlers)
+}
+
+func maintainConnections(s *connection.Server, clients *map[int]connection.Client, handlers *map[string]func(client connection.Client, data string)) {
+	for {
+		conn, _ := s.Ln.Accept()
+		ID := len(*clients) + 1
+		client := connection.Client{ID, 0, conn, make(chan string), make(chan string)}
+		(*clients)[ID] = client
+
+		utils.Log("User " + strconv.Itoa(client.ID) + " connected!")
+
+		go s.Speak(client)
+		go s.Listen(client)
+		go s.On(client, handlers)
+
+		client.From <- "connect|" + strconv.Itoa(client.ID)
+	}
 }
--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -3,87 +3,75 @@ package connection
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
+	"time"
 
 	"github.com/hramov/battleship_server/pkg/utils"
 )
 
 type Server struct {
-	protocol string
-	ip       string
-	port     string
-	ln       net.Listener
+	Protocol string
+	Ip       string
+	Port     string
+	Ln       net.Listener
 }
 
 type Client struct {
 	ID      int
 	EnemyID int
-	Name    string
 	Socket  net.Conn
 	From    chan string
 	To      chan string
 }
 
-func Execute(protocol, ip, port string, handler func(s *Server, client Client, clients map[int]Client)) {
+func Execute(protocol, ip, port string) *Server {
 	server := Server{protocol, ip, port, nil}
 	server.createServer()
-	clients := make(map[int]Client)
-	server.maintainConnections(clients, handler)
+	return &server
 }
 
-func (s *Server) createServer() {
-	s.ln, _ = net.Listen(s.protocol, s.ip+":"+s.port)
-	fmt.Println("Server is listening for connections on " + s.ip + ":" + s.port)
+func (s *Server) createServer() { // +
+	s.Ln, _ = net.Listen(s.Protocol, s.Ip+":"+s.Port)
+	fmt.Println("Server is listening for connections on " + s.Ip + ":" + s.Port)
 }
 
-func (s *Server) listen(client Client) {
+func (s *Server) Listen(client Client) {
 	for {
-		rawData, _ := bufio.NewReader(client.Socket).ReadString('\n')
+		rawData, err := bufio.NewReader(client.Socket).ReadString('\n')
+		if err != nil && err.Error() == "EOF" {
+			client.From <- string("disconnect|" + strconv.Itoa(client.ID))
+			return
+		} else if err != nil {
+			log.Fatal(err)
+		}
 		client.From <- rawData
 	}
 }
 
-func (s *Server) On(client Client, rawEvent string, callback func(data string)) {
-	rawData := <-client.From
-	event, data := utils.Split(rawData, "|")
-	if event == rawEvent {
-		callback(string(data))
-	}
-}
-
-func (s *Server) speak(client Client) {
+func (s *Server) On(client Client, handlers *map[string]func(client Client, data string)) {
 	for {
-		select {
-		case rawData := <-client.To:
-			{
-				event, data := utils.Split(rawData, "|")
-				client.Socket.Write([]byte(string(event) + "|" + string(data) + "\n"))
+		time.Sleep(time.Second / 1000)
+		rawData := <-client.From
+		rawEvent, data := utils.Split(rawData, "|")
+		for event, handler := range *handlers {
+			if event == rawEvent {
+				handler(client, data)
 			}
 		}
-		// rawData := <-client.To
-		// event, data := utils.Split(rawData, "|")
-		// client.Socket.Write([]byte(string(event) + "|" + string(data) + "\n"))
 	}
 }
 
-func (s *Server) Emit(client Client, event string, data string) {
-	client.To <- string(event + "|" + data)
-}
-
-func (s *Server) maintainConnections(clients map[int]Client, handler func(s *Server, client Client, clients map[int]Client)) {
+func (s *Server) Speak(client Client) {
 	for {
-		conn, _ := s.ln.Accept()
-		fmt.Println("New connection")
-
-		ID := len(clients) + 1
-		client := Client{ID, 0, "", conn, make(chan string, 10), make(chan string, 10)}
-		clients[ID] = client
-
-		go s.speak(client)
-		go handler(s, client, clients)
-		go s.listen(client)
-
-		client.From <- "connect|" + strconv.Itoa(client.ID)
+		rawData := <-client.To
+		event, data := utils.Split(rawData, "|")
+		client.Socket.Write([]byte(string(event) + "|" + string(data) + "\n"))
 	}
+}
+
+func (s *Server) Emit(client Client, event, data string) {
+	time.Sleep(time.Second / 1000)
+	client.To <- string(event + "|" + data)
 }
